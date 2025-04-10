98945bec701f9b6804eed8c1e0b06a034afea070
harish.vaithiyalingam <harish.vaithiyalingam@zohocorp.com>
2025-04-10 08:17:47 +0530

    demo commit-cicd

--- product-service/index.js
+++ product-service/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/product', (req, res) => res.send('Product service'));
+app.get('/product', (req, res) => res.send('Product service was started'));
 app.listen(3002, () => console.log('Product service on port 3002'));
